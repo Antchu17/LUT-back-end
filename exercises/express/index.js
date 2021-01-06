02f08ad74269170eb07b4384d44f30a5fb611b06
Antchu17 <hernesniemi.antti@outlook.com>
2021-01-06 21:25:45 +0200

    member id

--- exercises/express/index.js
+++ exercises/express/index.js
@@ -9,10 +9,21 @@ const app = express();
  
 
 // Init middleware
-app.use(logger);
+//app.use(logger);
 
 // Gets all members
 app.get('/api/members', (req, res) => res.json(members));
+
+// Get single Member
+app.get('/api/members/:id', (req, res) => {
+	const found = members.some(member => member.id === parseInt(req.params.id));
+
+	if(found){
+		res.json(members.filter(member => member.id === parseInt(req.params.id)));
+	}else{
+		res.status(400).json({msg: `No member with the id of ${req.params.id}`});
+	}
+});
 
 // Set a static folder
 app.use(express.static(path.join(__dirname, 'public')));
