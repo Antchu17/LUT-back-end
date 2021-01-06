9f2a7aaa696b4363bde7f86cb6fdec919393a461
Antchu17 <hernesniemi.antti@outlook.com>
2021-01-06 20:47:27 +0200

    html and css

--- exercises/ExpressJS/index.js
+++ exercises/ExpressJS/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { join } = require('path');
+const path = require('path');
 
 const app = express();
 
+// Set a static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = process.env.PORT || 5000;
 
-app.listen();
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
