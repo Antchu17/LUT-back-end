68adfeb16e666173010dd2796472e1f5f2dc98a7
Antchu17 <hernesniemi.antti@outlook.com>
2021-01-16 20:01:36 +0200

    subscriber

--- exercises/rest/routes/subscribers.js
+++ exercises/rest/routes/subscribers.js
@@ -1,10 +1,16 @@
 const { request } = require('express');
 const express = require('express');
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
 
 // Getting all
-router.get('/', (req, res) => {
-	res.send('Hellow world')
+router.get('/', async (req, res) => {
+	try {
+		const subscribers = await Subscriber.find()
+		res.json(subscribers)
+	}catch{
+		res.status(500).json({msg: err.message})
+	}
 })
 
 // Getting one
@@ -13,8 +19,18 @@ router.get('/:id', (req, res) => {
 })
 
 // Creating one
-router.post('/', (req, res) => {
-	
+router.post('/', async (req, res) => {
+	const subscriber = new Subscriber({
+		name: req.body.name,
+		subscribedToChannel: req.body.subscribedToChannel
+	})
+
+	try{
+		const newSubscriber = await subscriber.save()
+		res.status(201).json(newSubscriber)
+	}catch (err){
+		res.status(400).json({msg: err.message})
+	}
 })
 
 // Updating one
